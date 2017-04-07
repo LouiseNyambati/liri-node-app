14976635491d262c2a4d892abf7c0e7da1138fe3
Louise Nyambati <louisen1993@gmail.com>
2017-04-06 22:44:01 -0500

    initial commit

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '9onoLsPSfknZM1WMgLf9pCC4N',
+  consumer_secret: '	ZVZj0MJoBnLdK4VJ4wypbJV7qeh81QijHhTenQV4xyqV706wbi',
+  access_token_key: '	757914523714002944-gadqgTdyZlMlfHIc46o2yz6jpMHuzKo',
+  access_token_secret: '	dC0kqG7HLAYV3Q1fHapB0utTQvsqnqptr2qJ9mbC3JA4b',
+}
